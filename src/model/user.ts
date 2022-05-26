7216708db8ccc277948d8ed79959ce50323cd075
kimn <a9523023650@gmail.com>
2022-05-26 23:45:29 +0800

    [修改] userSchema

--- src/model/user.ts
+++ src/model/user.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 interface IUser {
     name: string;
+    sex: string;
     email: string;
     password: string;
     photo: string;
@@ -10,8 +11,9 @@ interface IUser {
 const userSchema = new mongoose.Schema<IUser>(
     {
         name: { type: String, required: [true, "name 未填寫"] },
+        sex: { type: String, enum: ["male", "female"] },
         email: { type: String, required: [true, "email 未填寫"] },
-        password: { type: String, required: [true, "password 未填寫"] },
+        password: { type: String, required: [true, "password 未填寫"], select: false },
         photo: { type: String, default: "https://i.imgur.com/tPmUQVM.png" },
     },
     { versionKey: false, timestamps: true }
