604a06bea5375a78fdc9975789a49f2282194316
kimn <a9523023650@gmail.com>
2022-05-30 00:09:33 +0800

    [修改] getImage 上傳到指定相簿

--- src/controller/images.ts
+++ src/controller/images.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import FormData from "form-data";
 import axios from "axios";
+import "dotenv/config";
 
 class ImageController {
     getImage = async (req: Request, res: Response): Promise<void> => {
@@ -9,8 +10,9 @@ class ImageController {
         }
         const formData = new FormData();
         formData.append("image", req.file.buffer);
-        const { data } = await axios.post("https://api.imgur.com/3/image", formData, {
-            headers: { Authorization: "Client-ID 62004dc8f2239f1" },
+        formData.append("album", process.env.IMGUR_ALBUM_ID);
+        const { data } = await axios.post(`https://api.imgur.com/3/image`, formData, {
+            headers: { Authorization: `Bearer ${process.env.IMGUR_REFRESH_TOKEN}` },
         });
         res.send({ status: "success", result: data.data.link });
     };
