4e3e4896f0891182215f69cc1f7d1307a7c8786d
kimn <a9523023650@gmail.com>
2022-06-13 22:40:34 +0800

    [新增] facebook 登入註冊

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { DotenvParseOutput, config } from "dotenv";
-import { Profile, Strategy as GoogleStrategy, VerifyCallback } from "passport-google-oauth20";
+import { Profile, VerifyCallback, Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as FacebookStrategy, VerifyFunction } from "passport-facebook";
 import passport from "passport";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
@@ -8,8 +9,15 @@ import * as Model from "../model";
 
 class AuthController {
     constructor() {
-        const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_LOGIN_CALL_BACK_URL } = config()
-            .parsed as DotenvParseOutput;
+        const {
+            GOOGLE_CLIENT_ID,
+            GOOGLE_CLIENT_SECRET,
+            GOOGLE_LOGIN_CALL_BACK_URL,
+            FACEBOOK_APP_ID,
+            FACEBOOK_APP_SECRET,
+            FACEBOOK_APP_CALLBACK,
+        } = config().parsed as DotenvParseOutput;
+
         passport.use(
             new GoogleStrategy(
                 {
@@ -18,6 +26,17 @@ class AuthController {
                     callbackURL: GOOGLE_LOGIN_CALL_BACK_URL,
                 },
                 this.loginByGoogle
+            )
+        );
+        passport.use(
+            new FacebookStrategy(
+                {
+                    clientID: FACEBOOK_APP_ID,
+                    clientSecret: FACEBOOK_APP_SECRET,
+                    callbackURL: FACEBOOK_APP_CALLBACK,
+                    profileFields: ["id", "displayName", "photos", "email"],
+                },
+                this.loginByFacebook
             )
         );
     }
@@ -36,7 +55,23 @@ class AuthController {
         return callback(null, result);
     };
 
-    googleCallback = async (req: Request, res: Response): Promise<void> => {
+    loginByFacebook: VerifyFunction = async (...args) => {
+        const [accessToken, refreshToken, profile, callback] = args;
+        const { id: facebookId, name, picture, email } = profile._json;
+        const user = await Model.Users.findOne({ facebookId });
+        if (user) {
+            return callback(null, user);
+        }
+        if (await Model.Users.findOne({ email })) {
+            throw new Error("此 Email 已被註冊!");
+        }
+        const password = await bcrypt.hash(facebookId, 12);
+        const photo = picture.data.url;
+        const result = await Model.Users.create({ name, email, password, facebookId, photo });
+        return callback(null, result);
+    };
+
+    loginCallback = async (req: Request, res: Response): Promise<void> => {
         const { _id, name } = req.user as any;
         const { JWT_SECRET, JWT_EXPIRES_DAY } = process.env as any;
         const token = jwt.sign({ userId: _id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_DAY });
